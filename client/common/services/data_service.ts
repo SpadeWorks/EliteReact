0be83e279eedd3e9d1121439135626f1282daefa
Akash Badhe <akash.badhe@asiapac.local>
2018-02-15 18:51:06 +0530

    Update code for user rank

--- client/common/services/data_service.ts
+++ client/common/services/data_service.ts
@@ -573,7 +573,7 @@ export class Services {
                     let points;
                     results.forEach((item, index) => {
                         if (item[Constants.Columns.USER_ID][Constants.Columns.ID] == userID) {
-                            rank = index;
+                            rank = index + 1;
                             points = item[Constants.Columns.POINTS];
                             return false;
                         }
