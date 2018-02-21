8ae579e039a6a59b07ec3035bebcb980bd6e4a3f
AkashBadhe <askext@gmail.com>
2018-02-21 20:24:28 +0530

    Updated site url for QA

--- client/onboarding/components/CurrentRide.tsx
+++ client/onboarding/components/CurrentRide.tsx
@@ -31,7 +31,7 @@ class CurrentRide extends React.Component<CurrentRideProps> {
             <p className="next-text">SHINY !</p>
                 <div id="typewriteText" style={{ animationDelay: "10s" }}></div>
                 <div className="shoping_cartbox">
-                    <img src="/sites/elite-dev-akash/Style%20Library/Elite/images/shoppingcart.png" />
+                    <img src="/sites/elite/Style%20Library/Elite/images/shoppingcart.png" />
                 </div>
             <div className="col-md-12 intro_actionbox testdrive_actionbox">
             <div className="button type1 pull-right">
