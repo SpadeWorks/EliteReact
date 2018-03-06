6b06a3bcfeecfaa7bd0adbf5ed4842a630443a0b
Akash Badhe <akash.badhe@asiapac.local>
2018-03-06 18:49:18 +0530

    updated row class for role

--- client/profile/components/MyProfileLeftContainer.tsx
+++ client/profile/components/MyProfileLeftContainer.tsx
@@ -12,14 +12,15 @@ class MyProfileLeftContainer extends React.Component<MyProfileLeftContainerProps
     render() {
         const { eliteProfile } = this.props;
         return (
-            <div className="col-md-6  profileinfo_box" style={{ borderRight: "solid 1px #3c3c3c"}}>
+            <div className="col-md-6  profileinfo_box" style={{ borderRight: "solid 1px #3c3c3c" }}>
                 <div className="row inforow">
                     <div className="col-md-5">
                         <span className="orange">Role :</span>
                     </div>
                     <div className="col-md-6">
-                    
-                        <h5>{eliteProfile.role}</h5>
+                        <div className="row">
+                            <h5>{eliteProfile.role}</h5>
+                        </div>
                     </div>
                 </div>
                 <div className="row inforow">
@@ -27,8 +28,8 @@ class MyProfileLeftContainer extends React.Component<MyProfileLeftContainerProps
                         <span className="orange">Region :</span>
                     </div>
                     <div className="col-md-6">
-                    <div className="row">
-                        <h5>{eliteProfile.region}</h5>
+                        <div className="row">
+                            <h5>{eliteProfile.region}</h5>
                         </div>
                     </div>
                 </div>
@@ -37,8 +38,8 @@ class MyProfileLeftContainer extends React.Component<MyProfileLeftContainerProps
                         <span className="orange">Location :</span>
                     </div>
                     <div className="col-md-6">
-                    <div className="row">
-                        <h5>{eliteProfile.location}</h5>
+                        <div className="row">
+                            <h5>{eliteProfile.location}</h5>
                         </div>
                     </div>
                 </div>
@@ -47,9 +48,9 @@ class MyProfileLeftContainer extends React.Component<MyProfileLeftContainerProps
                         <span className="orange">Joined Date :</span>
                     </div>
                     <div className="col-md-6">
-                    <div className="row">
-                        <h5>{eliteProfile.dateJoined}</h5>
-                    </div>
+                        <div className="row">
+                            <h5>{eliteProfile.dateJoined}</h5>
+                        </div>
                     </div>
                 </div>
             </div>
