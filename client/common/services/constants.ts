ea8e29cfe4a8169d4baa1c2b5a717e111ffb4b45
swapnilphatale <phatale.swapnil@gmail.com>
2018-03-09 22:04:53 +0530

    Pop up updates

--- client/common/services/constants.ts
+++ client/common/services/constants.ts
@@ -155,14 +155,14 @@ export class Messages {
     static SHARE_TITLE = 'Share';
     static START_GREATOR_ERROR = 'Start date can not be greator than end date';
     static POP_THE_FIZZ_1 = "You have successfully completed this testdrive.";
-    static POP_THE_FIZZ_2 = "You have completed all the test cases and answered #0# of #1# survey questions."
-    static POP_THE_FIZZ_3 = "You earned #0# points.";
-    static HIGH_FIVE_1 = "You have completed #0# of #1# test cases successfully.";
-    static HIGH_FIVE_2 = "You earned #0# points."
-    static HIGH_FIVE_3 = "You are now #0# on the leaderboard.";
-    static HIGH_FIVE_4 = "You are #0# points away from getting a #1#";
-    static MISSING_OUT_1 = "You have completed #0# of #1# test cases.";
-    static MISSING_OUT_2 = "You earned #0# points."
-    static MISSING_OUT_3 = "You are now #0# on the leaderboard.";
-    static MISSING_OUT_4 = "You are #0# points away from getting a #1#";
+    static POP_THE_FIZZ_2 = "You have completed all the test cases and answered <span class='orange'>#0#</span> of #1# survey questions."
+    static POP_THE_FIZZ_3 = "You earned <span class='orange'>#0#</span> points.";
+    static HIGH_FIVE_1 = "You have completed <span class='orange'>#0#</span> of #1# test cases successfully.";
+    static HIGH_FIVE_2 = "You earned <span class='orange'>#0#</span> points."
+    static HIGH_FIVE_3 = "You are now <span class='orange'>#0#</span> on the leaderboard.";
+    static HIGH_FIVE_4 = "You are <span class='orange'>#0#</span> points away from getting a #1#";
+    static MISSING_OUT_1 = "You have completed <span class='orange'>#0#</span> of #1# test cases.";
+    static MISSING_OUT_2 = "You earned <span class='orange'>#0#</span> points."
+    static MISSING_OUT_3 = "You are now <span class='orange'>#0#</span> on the leaderboard.";
+    static MISSING_OUT_4 = "You are <span class='orange'>#0#</span> points away from getting a #1#";
 }
